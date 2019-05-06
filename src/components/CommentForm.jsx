ddabc4dcdf803f9659671504c03359862e80cfc5
inwisloklem <89099109104@mail.ru>
2019-05-06 23:15:00 +0300

    Update (26)

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styles from 'components/CommentForm.module.sass'
 
-function CommentForm () {
-  return (
-    <form className={styles.form}>
-      <textarea
-        className={styles.textArea}
-        placeholder='Enter your comment here...'
-      />
-      <button className={styles.button} type='submit'>
-        Add
-      </button>
-    </form>
-  )
+class CommentForm extends Component {
+  state = { comment: '' }
+
+  handleChange = e => {
+    this.setState({ comment: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    this.setState({ comment: '' })
+  }
+
+  render () {
+    const { comment } = this.state
+    const { handleChange, handleSubmit } = this
+    return (
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <textarea
+          className={styles.textArea}
+          onChange={handleChange}
+          placeholder='Enter your comment here...'
+          value={comment}
+        />
+        <button className={styles.button} type='submit'>
+          Add
+        </button>
+      </form>
+    )
+  }
 }
 
 export default CommentForm
